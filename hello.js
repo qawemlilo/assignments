2f9536983b2e04c59a1bc72aeca4f68ee0c28f31
Qawelesizwe <qawemlilo@gmail.com>
2013-07-11 16:03:06 +0200

    First commit

---
+++ hello.js
@@ -0,0 +1,8 @@
+﻿var fs = require('fs');
+
+try {
+    fs.writeFileSync("./hello.txt", "A startup is a business built to grow rapidly.\n", "utf8");
+    console.log('Done!');
+} catch (error) {
+    throw error;
+}
---
+++ hello2.js
@@ -0,0 +1,67 @@
+﻿
+/*
+   Global variable
+*/
+
+var fs = require('fs');
+
+
+function isPrime (num) {
+    var m;
+    if (num < 2) {
+        return false;
+    }
+    
+    if (num % 2 === 0) {
+        return (num === 2);
+    }
+    
+    if (num % 3 === 0) {
+        return (num === 3);
+    }
+ 
+    m = Math.sqrt(num);
+    
+    for (var i = 5; i <= m; i += 6) {
+    
+        if (num % i === 0) {
+            return false;
+        }
+        
+        if (num % (i + 2) === 0) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+
+
+function createFile (filename, data) {
+    try {
+        fs.writeFileSync(filename, data, "utf8");
+        console.log('Done!');
+    } catch (error) {
+        throw error;
+    }
+}
+
+
+
+function main () {
+    var counter = 2, 
+        primes = [];
+    
+    while(primes.length < 100) {
+        if (isPrime(counter)) {
+            primes.push(counter)
+        }
+    
+        counter += 1;
+    }
+    
+    createFile('./hello2.txt', primes.join(','))
+}
+
+main();
